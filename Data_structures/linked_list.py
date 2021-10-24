d86aa2b5f39afcac2be5b05c30f858c2dc3c0877
Kupcor <kupcor1998@gmail.com>
2021-10-24 21:45:43 +0200

    Add insert, remove and delete method to Linked List class

--- Data_structures/linked_list.py
+++ Data_structures/linked_list.py
@@ -1,6 +1,10 @@
 # ========= Info =========
 # This file contain basic of the linked list in python
 
+# ========= Directories =========
+from Rand_numbers import rand_numbers
+
+
 # Single linked list
 # Creating class to represent a node
 class Node:
@@ -36,7 +40,7 @@ class LinkedList:
         # current variable is a pointer to the head node
         current = self.head
 
-        while current: # if current is not empty then while loop will execute
+        while current:  # if current is not empty then while loop will execute
             if current is self.head:
                 nodes.append(("[Head: %s]" % current.data))  # Node which is head will be featured
             elif current.next_node is None:  # Node which is tail will be feature
@@ -48,6 +52,90 @@ class LinkedList:
             current = current.next_node
 
         return '-> '.join(nodes)  # double string return
+
+    # method to search item in linked list
+    # if value is present in linked list method will return True
+    # else method will return None
+    # O(n) time
+    def search(self, key):
+        current = self.head
+
+        while current:  # loop will run only if linked list exist, if it run to Tail it will end to
+            if current.data == key:
+                return True
+            # if key is not found in
+            else:
+                current = current.next_node
+        #  if searched value does not exist method will return None
+        return None
+
+    # insert method -> it will insert any values between any nodes in linked list
+    # index is not exactly an index position of element in Linked list like in arrays, but
+    # this serves as a countdown from the start to an position
+    # overall it takes linear time O(n)
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+        elif index > self.size():
+            return "Error: index values is greater tha linked list size"
+        elif index > 0:
+            new_node = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new_node
+            new_node.next_node = next_node
+
+    # remove method -> it will delete first delivered value in linked list
+    # in worse case scenario it takes linear time
+    def remove(self, key):
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            # we want to delete head of the list
+            if current.data == key and current == self.head:
+                self.head = current.next_node
+                found = True
+
+            # if key is not a head
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            # if key value is not found
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+
+    # delete method -> deleting method by Node position (index variable)
+    def delete(self, index):
+        current = self.head
+
+        # First case -> we want to delete head
+        if index == 0:
+            self.head = current.next_node
+        elif index > self.size():
+            return "Error: this position does not exist in this linked list"
+        elif index > 0:
+            position = index
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            next_value = current.next_node
+            current.next_node = next_value.next_node
 
     # method to check if LinkedList is empty or not
     def is_empty(self):
@@ -81,10 +169,20 @@ class LinkedList:
 
 
 l = LinkedList()
-l.add(10)
-l.add(13)
-l.add(34)
-size = l.size()
-print(size)
-list = l.__repr__()
-print(list)
+list_of_values = rand_numbers.rand_number(10, 10)
+for i in range(0, len(list_of_values)):
+    l.add(list_of_values[i])
+
+print(l)
+l.insert(100, 5)
+print(l)
+l.remove(5)
+print(l)
+l.delete(2)
+print(l)
+l.delete(4)
+result = l.insert(100, 20)
+print(result)
+result = l.size()
+print(result)
+print(l)
